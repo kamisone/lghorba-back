58ecad55b274e04307b6b7f45ebd7b2d97134755
mbk <mbk@gmail.com>
2026-04-30 22:54:59 +0200

    removed bodyparser interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
-import * as bodyParser from 'body-parser';
 
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
-  app.use(bodyParser.json({ type: ['application/json', 'text/plain'] }));
   app.enableCors({ origin: '*' });
   await app.listen(process.env.BACK_PORT || 3000);
 }
